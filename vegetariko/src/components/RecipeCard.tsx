a0c3fc2e8a6f1bf3bc92c33b6e7f89a42daa8aa4
SerenaGiumentaro <serena.giumentaro@gmail.com>
2024-03-11 19:56:00 +0100

    create recipe card

---
+++ vegetariko/src/components/RecipeCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Panel from 'rsuite/esm/Panel'
+
+type RecipeCardProps = {
+    title: string;
+    imgUrl: string;
+}
+const RecipeCard:React.FC<RecipeCardProps> = ({title, imgUrl}) => {
+  return (
+    <Panel shaded bordered bodyFill style={{ display: 'inline-block', width: 240 }}>
+    <img src={imgUrl} height="240" />
+    <Panel header={title} >
+      
+    </Panel>
+  </Panel>
+  )
+}
+
+export default RecipeCard

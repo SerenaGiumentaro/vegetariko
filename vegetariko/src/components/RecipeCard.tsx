a8a5cc5b38633c8d03145d439677f8e139415e89
SerenaGiumentaro <serena.giumentaro@gmail.com>
2024-03-12 10:31:12 +0100

    set pagination

--- vegetariko/src/components/RecipeCard.tsx
+++ vegetariko/src/components/RecipeCard.tsx
@@ -7,7 +7,7 @@ type RecipeCardProps = {
 }
 const RecipeCard:React.FC<RecipeCardProps> = ({title, imgUrl}) => {
   return (
-    <Panel shaded bordered bodyFill style={{ display: 'inline-block', width: 240 }}>
+    <Panel shaded bordered bodyFill style={{ display: 'inline-block', width: 240, height: 280}}>
     <img src={imgUrl} height="240" />
     <Panel header={title} >
       
--- vegetariko/src/services/apiService.ts
+++ vegetariko/src/services/apiService.ts
@@ -1,10 +1,34 @@
 import axios from "axios";
 
-export const getRecipesBySearch = async (query:string) => {
+export const getRecipesBySearch = async (query: string, offset: number) => {
   try {
-    const response = await axios.get(`${import.meta.env.VITE_BASE_URL}${query}`, {
-      headers: { "x-api-key": `${import.meta.env.VITE_API_KEY}` },
-    });
+    const response = await axios.get(
+      `${import.meta.env.VITE_BASE_URL}${
+        import.meta.env.VITE_SEARCH_URL
+      }${offset}&query=${query}`,
+      {
+        headers: { "x-api-key": `${import.meta.env.VITE_API_KEY}` },
+      }
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Errore nel caricamnto dei dati", error);
+    throw new Error();
+  }
+};
+
+
+export const getRecipes = async (offset: number) => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_BASE_URL}${
+        import.meta.env.VITE_SEARCH_URL
+      }${offset}`,
+      {
+        headers: { "x-api-key": `${import.meta.env.VITE_API_KEY}` },
+      }
+    );
     console.log(response.data);
     return response.data;
   } catch (error) {

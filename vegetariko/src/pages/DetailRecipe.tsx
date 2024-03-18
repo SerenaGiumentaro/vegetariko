7f5a59efaff4a364338ed7757efa6a91bf24b35e
SerenaGiumentaro <serena.giumentaro@gmail.com>
2024-03-18 19:05:17 +0100

    loader in detail recipe

--- vegetariko/src/pages/DetailRecipe.tsx
+++ vegetariko/src/pages/DetailRecipe.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { RecipeInfo } from "../types";
 import { getRecipeByID } from "../services/apiService";
+import { ButtonGroup, IconButton, Loader } from "rsuite";
+import ArowBackIcon from "@rsuite/icons/ArowBack";
 
 const DetailRecipe = () => {
   const { id } = useParams();
   const [recipe, setRecipe] = React.useState<RecipeInfo>();
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const nav = useNavigate();
 
   React.useEffect(() => {
     const fetchRecipeData = async () => {
@@ -14,15 +18,35 @@ const DetailRecipe = () => {
         setRecipe(recipe);
       } catch (err) {
         console.error("Errore nel caricamento dati", err);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchRecipeData();
   }, []);
 
-  return recipe ? (
+  if (isLoading) {
+    return <Loader backdrop content="loading..." vertical />;
+  }
+
+  if (!recipe) {
+    return (
+      <>
+        <ButtonGroup onClick={() => nav("/")}>
+          <IconButton icon={<ArowBackIcon />} />
+        </ButtonGroup>
+        <p>There are no data for this recipe.</p>;
+      </>
+    );
+  }
+
+  return (
     <div>
+      <ButtonGroup onClick={() => nav("/")}>
+        <IconButton icon={<ArowBackIcon />} />
+      </ButtonGroup>
       <h1>{recipe.title}</h1>
-      <h2>Score: {parseFloat((recipe.spoonacularScore).toFixed(2))}</h2>
+      <h2>Score: {parseFloat(recipe.spoonacularScore.toFixed(2))}</h2>
       <img src={recipe.image} alt={recipe.title} />
       <p>Ready in {recipe.readyInMinutes} minutes.</p>
       <p>Ingredients for {recipe.servings} people:</p>
@@ -36,13 +60,13 @@ const DetailRecipe = () => {
       <div dangerouslySetInnerHTML={{ __html: recipe.summary }}></div>
       <div>
         <h5>Instructions:</h5>
-      <div dangerouslySetInnerHTML={{ __html: recipe.instructions }}></div>
+        <div dangerouslySetInnerHTML={{ __html: recipe.instructions }}></div>
       </div>
-      <p>Source:
-        <a href={recipe.sourceUrl}>{recipe.sourceUrl}</a> </p>
+      <p>
+        Source:
+        <a href={recipe.sourceUrl}>{recipe.sourceUrl}</a>{" "}
+      </p>
     </div>
-  ) : (
-    <p>There are no recipe data</p>
   );
 };
 
